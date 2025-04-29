b8405697062c142a429724e741f0c9293d025604
Monty King <55464807+mixchi@users.noreply.github.com>
2025-04-28 21:25:36 -0600

    grid size method

---
+++ utils/gridutils.py
@@ -0,0 +1,14 @@
+import glob
+import sys
+import numpy as np
+
+def checkGridSize(file):
+    # load csv
+    data = np.genfromtxt(file, delimiter=',')
+    print(file+"\t"+str(data.shape))
+
+def main():
+    for file in glob.glob(sys.argv[1]+"/*"):
+        checkGridSize(file)
+
+if __name__ == "__main__": main() 
