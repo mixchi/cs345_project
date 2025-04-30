bc586065124bf402f0cfcc854cda253ab769891a
Monty King <55464807+mixchi@users.noreply.github.com>
2025-04-29 23:07:00 -0600

    remove redundant columns of lat, lon, and time in main grid

--- h5utils/gridutils/process.py
+++ h5utils/gridutils/process.py
@@ -5,8 +5,8 @@ def process(h5obj):
     moleculeFilter = h5obj.getFilter()
     filtered = moleculeFilter.filterGrid(h5obj)
 
-    bigtable = np.concatenate((filtered, h5obj.getLats()[:,None]), axis=1)
-    bigtable = np.concatenate((bigtable, h5obj.getLons()[:,None]), axis=1)
-    bigtable = np.concatenate((bigtable, h5obj.getTime()[:,None]), axis=1)
+    # bigtable = np.concatenate((filtered, h5obj.getLats()[:,None]), axis=1)
+    # bigtable = np.concatenate((bigtable, h5obj.getLons()[:,None]), axis=1)
+    # bigtable = np.concatenate((bigtable, h5obj.getTime()[:,None]), axis=1)
 
-    return bigtable
+    return filtered
