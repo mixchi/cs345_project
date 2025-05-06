8feeb72ce1105c43d0a13e59c014f93d37ae2f45
Monty King <55464807+mixchi@users.noreply.github.com>
2025-05-05 22:42:58 -0600

    run only using generated npy file

--- ml/svm.py
+++ ml/svm.py
@@ -9,15 +9,13 @@ sys.path.append(str(Path(__file__).parent.absolute().parent.absolute()))
 import h5utils
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 3):
-        print("Usage: "+str(sys.argv[0])+" <inputDir> <outputDir>")
+    if (len(sys.argv) != 2):
+        print("Usage: "+str(sys.argv[0])+" <inputnpy>")
         sys.exit(1)
 
     gridloader = h5utils.dataInitializer()
 
-    npydata = gridloader.processGrids(sys.argv[1], sys.argv[2])
-
     # load an np array of h5file objects
-    h5array = gridloader.loadh5(npydata)
+    h5array = gridloader.loadh5(sys.argv[1])
 
     print("Number of h5files: "+str(h5array.size))
